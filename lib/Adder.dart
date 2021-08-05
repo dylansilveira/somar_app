d724105dad71aa0a4db722fc4e02e81d57cc3fe3
Dylan Silveira <contato.dylansilveira@gmail.com>
2021-08-05 13:20:39 -0300

    First commit

---
+++ lib/Adder.dart
@@ -0,0 +1,9 @@
+class Adder{
+  int num1, num2;
+
+  Adder(this.num1, this.num2);
+
+  int soma(){
+    return num1 + num2;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'Adder.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Somar'),
+        ),
+        body: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  Adder ad = Adder(0, 0);
+  int soma = 0;
+
+  @override
+  Widget build(BuildContext context) {
+
+    void _somar(){
+      setState(() {
+        soma = ad.soma();
+      });
+    }
+
+    return Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Primeiro número',
+            ),
+            onChanged: (value) => ad.num1 = int.parse(value),
+            keyboardType: TextInputType.number,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Segundo número',
+            ),
+            onChanged: (value) => ad.num2 = int.parse(value),
+            keyboardType: TextInputType.number,
+          ),
+          ElevatedButton(
+              onPressed: _somar,
+              child: Text('Somar'),
+          ),
+          Text('Resultado da soma: $soma'),
+      ],
+    );
+  }
+}
